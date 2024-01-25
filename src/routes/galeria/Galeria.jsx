8e5406fe4eae98af6142c31ab8ed00828f2cdbb0
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2024-01-25 21:10:31 +0100

    Css basico en Galeria.css movil size

--- src/routes/galeria/Galeria.jsx
+++ src/routes/galeria/Galeria.jsx
@@ -6,13 +6,16 @@ export default function Galeria() {
     return (
         <div className='galeriaGeneral'>
             <div className='galeriaFoto'>
-                <p>logo congregacio</p>
+                <p>aqui va la foto</p>
             </div>
             <div className='galeriaExpli'>
                 <h2>Galeria</h2>
-                <p>
-                    Totes les fotos
-                </p>
+                <div className='foto1'></div>
+                <div className='foto2'></div>
+                <div className='foto3'></div>
+                <div className='foto4'></div>
+                <div className='foto5'></div>
+                <div className='foto6'></div>
             </div>
             <div className="galeriaReturn">
                 <Link to="/">Inici</Link>
