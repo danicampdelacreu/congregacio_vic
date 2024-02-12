e273f6cd2170bcf7a31bef62a9a1e62b14d477e9
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2024-02-12 17:06:18 +0100

    Ajust Espiritual

--- src/routes/espiritual/Espiritual.jsx
+++ src/routes/espiritual/Espiritual.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 
 import "./Espiritual.css"
 
@@ -11,6 +12,10 @@ export default function Espirtual() {
             <div className="espiritualEspais">
                 <h3>Espais</h3>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt consectetur deleniti praesentium, expedita officia dolor, deserunt soluta labore voluptatem unde suscipit molestias autem sint adipisci, reiciendis quia nemo perspiciatis amet?</p>
+            </div>
+
+            <div className="espiritualReturn">
+                <Link to="/">Inici</Link>
             </div>
         </div>
     )
