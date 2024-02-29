8d1213aa0847cfea5a33167fee58d0f50abb0e5a
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2024-02-29 18:24:12 +0100

    home, footer, header, processor size movil & tablet V

--- src/routes/processo/Processo.jsx
+++ src/routes/processo/Processo.jsx
@@ -13,17 +13,18 @@ export default function Processo() {
             <div className="processoFoto"></div>
 
             <div className="processoTitul">
-                <h2>Processó</h2>
+                <h2>PROCESSÓ</h2>
             </div>
 
             <div className="processoDia">
-                <p>Diumenge, 24 de Març</p>
+                <p>Diumenge, 24 de Març del 2024</p>
             </div>
 
             <div className="processoExpli">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam distinctio similique assumenda, nostrum dolorem officiis temporibus ea harum quo, sed eius repudiandae eligendi veritatis cumque ipsa nemo perspiciatis! Aliquam, rerum.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nemo minima ex quas id vero, pariatur, atque voluptate repellat temporibus quam sint autem iusto deleniti commodi fuga. Sed, neque et.
-                </p>
+                <p>La primera referència a la Processó dels Armats o del Silenci, és de 1750. Aquesta és, segons els experts, la manifestació piatosa que millor ha guardat les formes tradicionals i, per tant, un interessant viatge al segle XVIII.</p>
+                <p>Pocs han estat els anys en què no s'ha celebrat, per causa de la pluja o de pandèmies. El període més llarg va ser des del 1973 al 1981, any en què uns joves i la voluntat de la gent, varen ajudar a recuperar-la i amb una gran assistència.</p>
+                <p>La Processó, no ha restat immòbil als canvis del nostre temps. Essent oberta a totes les persones, sense limitació de cap mena.</p>
+                <p>L'ambient dels carrers del centre històric de la ciutat amb el foc, els papus i els armats i aquell silenci, trencat només per les llances picant a terra i el cant de l'Stabat Mater crea una atmosfera molt especial. Fins al punt que gent d'arreu participa d'aquest acte multitudinari, que agrupa a unes 500 persones.</p>
             </div>
 
             <div className='processoFotos'>
