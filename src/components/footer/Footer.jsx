2315e486ed75b0741723bd643a14abeac2fc46f7
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2024-03-07 14:49:51 +0100

    maps, insta,

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom'
 
-import Insta from '../../assets/img/instalogo.png'
-
 import './Footer.css'
 
 import Aj from '../../assets/img/Aj.Vic.png';
@@ -9,6 +7,8 @@ import FG from '../../assets/img/f.Girbau.png';
 import FP from '../../assets/img/f.Puig.png';
 import FV from '../../assets/img/f.Vicus.png';
 import AD from '../../assets/img/a.Ausa.png';
+import Insta from '../../assets/img/instalogo.png'
+import maps from '../../assets/img/maps.png'
 
 
 export default function Footer() {
@@ -16,10 +16,14 @@ export default function Footer() {
     return (
         <div className='footerGeneral'>
             <div className='insta'>
-                <Link to="https://www.instagram.com/campusuegurb/">
+                <Link to="https://www.instagram.com/elsdolorsdevic/">
                     <img className="logoIH" src={Insta} />
                 </Link>
-                <p>@Congracacio dels dolors Vic</p>
+                <p>@elsdolorsdevic</p>
+                <Link to="https://www.google.es/maps/place/Carrer+dels+Dolors,+5,+08500+Vic,+Barcelona/@41.9281302,2.2562312,17z/data=!3m1!4b1!4m6!3m5!1s0x12a52705908dbda1:0xc2f9a920cdf93da3!8m2!3d41.9281302!4d2.2562312!16s%2Fg%2F11c243d07k?entry=ttu">
+                    <img className="logoLF" src={maps} />
+                </Link>
+                <p>C/ dels Dolors nº5 de Vic</p>
             </div>
 
             <div className="footerPatro">
