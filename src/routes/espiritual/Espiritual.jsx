bf3e8fdd5c3a8c748cd1c4a93c771f2bef6d1d42
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2024-03-13 16:51:52 +0100

    css adapt & espi form

--- src/routes/espiritual/Espiritual.jsx
+++ src/routes/espiritual/Espiritual.jsx
@@ -9,24 +9,23 @@ export default function Espirtual() {
     };
 
     return (
-        <div className="espiritualGeneral">
+        <div className='espiritualMain'>
+            <div className="espiritualGeneral">
+                <div className="espiritualCelebracions">
+                    <h2>CELEBRACIONS</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt consectetur deleniti praesentium, expedita officia dolor, deserunt soluta labore voluptatem unde suscipit molestias autem sint adipisci, reiciendis quia nemo perspiciatis amet?</p>
+                </div>
 
-            <div className="espiritualCelebracions">
-                <h2>CELEBRACIONS</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt consectetur deleniti praesentium, expedita officia dolor, deserunt soluta labore voluptatem unde suscipit molestias autem sint adipisci, reiciendis quia nemo perspiciatis amet?</p>
+                <div className="espiritualEspais">
+                    <h2>ESPAIS</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt consectetur deleniti praesentium, expedita officia dolor, deserunt soluta labore voluptatem unde suscipit molestias autem sint adipisci, reiciendis quia nemo perspiciatis amet?</p>
+                </div>
             </div>
-
-            <div className="espiritualEspais">
-                <h2>ESPAIS</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt consectetur deleniti praesentium, expedita officia dolor, deserunt soluta labore voluptatem unde suscipit molestias autem sint adipisci, reiciendis quia nemo perspiciatis amet?</p>
-            </div>
-
             <div className="espiritualReturn">
                 <Link to="/" onClick={returnClick}>
                     Inici
                 </Link>
             </div>
-
         </div>
     )
 }
