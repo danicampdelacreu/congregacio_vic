824eb4552320efef424375ced62bb44b25408824
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2024-03-14 16:24:42 +0100

    Adjust css client, intro photo galeria

--- src/routes/galeria/Galeria.jsx
+++ src/routes/galeria/Galeria.jsx
@@ -2,6 +2,10 @@ import { Link } from 'react-router-dom';
 
 import "./Galeria.css"
 
+import F1 from '../../assets/img/Galeria1.jpg'
+import F2 from '../../assets/img/Galeria2.jpg'
+import F3 from '../../assets/img/Galeria3.jpg'
+
 export default function Galeria() {
 
     const returnClick = () => {
@@ -16,28 +20,15 @@ export default function Galeria() {
             <div className='galeriaExpli'>
                 <h2>GALERIA DE FOTOS</h2>
                 <div className='galeriaA'> 
-                <h4>2015</h4>
+                <h4></h4>
                 </div>
                 <div className='carpetaFotos'>
-                    <div className='gfoto1'>
-                        <h3>Foto 1</h3>
-                    </div>
-                    <div className='gfoto2'>
-                        <h3>Foto 2</h3>
-                    </div>
-                    <div className='gfoto3'>
-                        <h3>Foto 3</h3>
-                    </div>
-                    <div className='gfoto4'>
-                        <h3>foto 4</h3>
-                    </div>
-                    <div className='gfoto5'>
-                        <h3>Foto 5</h3>
-                    </div>
-                    <div className='gfoto6'>
-                        <h3>Foto 6</h3>
-                    </div>
-
+                    <img className='gfoto1'src={F1}/>
+                    <img className='gfoto2'src={F2}/> 
+                    <img className='gfoto3'src={F3}/> 
+                    <img className='gfoto1'src={F1}/>   
+                    <img className='gfoto2'src={F2}/> 
+                    <img className='gfoto3'src={F3}/>   
                 </div>
 
             </div>
