a57d539824b565c6ee45f49d9f2c1b4d31a5de68
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2024-03-22 16:04:33 +0100

    Bug padding error white, className Home

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -10,53 +10,53 @@ export default function Home() {
     };
 
     return (
-        <main class='allMain'>
+        <main className='allMain'>
 
             <div className='homeFoto'></div>
 
-            <div class='homeProcesso'>
+            <div className='homeProcesso'>
                 <div className='homeProcessoIn'>
                     <h3>Processó</h3>
                     <Link to="/Processo" onClick={scrollToTop}>+ Info</Link>
                 </div>
             </div>
 
-            <div class='homeHistoria'>
+            <div className='homeHistoria'>
                 <div className='homeHistoriaIn'>
-                    <h3>Historia</h3>
+                    <h3>Història</h3>
                     <Link to="/Historia" onClick={scrollToTop}>+ Info</Link>
                 </div>
             </div>
 
-            <div class='homeNoticias'>
+            <div className='homeNoticias'>
                 <div className='homeNoticiasIn'>
                     <h3>Notícies</h3>
                     <Link to="/Noticias" onClick={scrollToTop}>+ Info</Link>
                 </div>
             </div>
 
-            <div class='homeGaleria'>
+            <div className='homeGaleria'>
                 <div className='homeGaleriaIn'>
                     <h3>Galeria</h3>
                     <Link to="/Galeria" onClick={scrollToTop}>+ Info</Link>
                 </div>
             </div>
 
-            <div class='homeDonativo'>
+            <div className='homeDonativo'>
                 <div className='homeDonativoIn'>
                     <h3>Ajuda al patrimoni</h3>
                     <Link to="/Donativo" onClick={scrollToTop}>+ Info</Link>
                 </div>
             </div>
 
-            <div class='homeEspiritual'>
+            <div className='homeEspiritual'>
                 <div className='homeEspiritualIn'>
                     <h3>Espiritualitat</h3>
                     <Link to="/Espiritual" onClick={scrollToTop}>+ Info</Link>
                 </div>
             </div>
 
-            <div class='homeInformacion'>
+            <div className='homeInformacion'>
                 <div className='homeInformacionIn'>
                     <h3>Informació General</h3>
                     <Link to="/Informacion" onClick={scrollToTop}>+ Info</Link>
